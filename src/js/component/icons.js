e5702931e640ff74f7886d24b7ba494aa3432788
davidmorasalazar <davidmsalazar99@gmail.com>
2021-02-05 03:08:52 +0000

    my commit 2

---
+++ src/js/component/icons.js
@@ -0,0 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-regular/faClock';
+fontawesome.config = {
+    autoReplaceSvg: "nest"
+}:
+fontawesome.library.add(faClock);
