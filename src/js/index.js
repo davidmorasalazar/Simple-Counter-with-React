c56ef9d6eb130e290c3f1f72072dec1fc355c01d
davidmorasalazar <davidmsalazar99@gmail.com>
2021-02-05 05:56:27 +0000

    my commit 3

--- src/js/index.js
+++ src/js/index.js
@@ -17,34 +17,75 @@ function SimpleCounter(props) {
 			<div className="calendar">
 				<i className="far fa-clock" />
 			</div>
-			<div className="four">{props.digitFour}</div>
-			<div className="three">{props.digitThree}</div>
-			<div className="two">{props.digitTwo}</div>
-			<div className="one">{props.digitOne}</div>
+			<div className="six">{props.digitSix % 10}</div>
+			<div className="five">{props.digitFive % 10}</div>
+			<div className="puntos">{props.digitPuntos}</div>
+			<div className="four">{props.digitFour % 10}</div>
+			<div className="three">{props.digitThree % 10}</div>
+			<div className="puntos">{props.digitPuntos}</div>
+			<div className="two">{props.digitTwo % 10}</div>
+			<div className="one">{props.digitOne % 10}</div>
 		</div>
 	);
 }
 SimpleCounter.PropTypes = {
+	digitSix: PropTypes.number,
+	digitFive: PropTypes.number,
 	digitFour: PropTypes.number,
 	digitThree: PropTypes.number,
 	digitTwo: PropTypes.number,
+	digitPuntos: PropTypes.string,
 	digitOne: PropTypes.number
 };
-let counter = 0;
+let counter1 = 0;
+let counter2 = 0;
+let counter3 = 0;
+let counter4 = 0;
+let counter5 = 0;
+let counter6 = 0;
 setInterval(function() {
-	const four = Math.floor(counter / 1000);
-	const three = Math.floor(counter / 100);
-	const two = Math.floor(counter / 10);
-	const one = Math.floor(counter / 1);
-	console.log(four, three, two, one);
-	counter++;
+	const six = Math.floor(counter6 / 1);
+	const five = Math.floor(counter5 / 1);
+	const four = Math.floor(counter4 / 1);
+	const three = Math.floor(counter3 / 1);
+	const two = Math.floor(counter2 / 1);
+	const puntos = ":";
+	const one = Math.floor(counter1 / 1);
+	if (one == 9) {
+		counter1 = 0;
+		counter2++;
+	}
+	if (two == 6) {
+		counter2 = 0;
+		counter3++;
+	}
+	if (three == 9) {
+		counter3 = 0;
+		counter4++;
+	}
+	if (four == 6) {
+		counter4 = 0;
+		counter5++;
+	}
+	if (five == 9) {
+		counter5 = 0;
+		counter6++;
+	}
+	console.log(six, five, four, three, two, puntos, one);
+	counter1++;
+	// counter2++;
+	// counter3++;
+	// counter4++;
 	//render your react application
 	ReactDOM.render(
 		<SimpleCounter
 			digitOne={one}
+			digitPuntos={puntos}
 			digitTwo={two}
 			digitThree={three}
 			digitFour={four}
+			digitFive={five}
+			digitSix={six}
 		/>,
 		document.querySelector("#app")
 	);
